6e803ba879836a2de99a479d3f6ef4f7948e76ad
Nastya <44947315+oeileyo@users.noreply.github.com>
2022-03-10 09:52:34 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.get('/', function (req, res){
-    res.sendFile('/Users/Anastasia/Desktop/Учеба/5 и 6 семестр/WEB/new_10_02/index.html')
-});
-
-app.listen(8089);
